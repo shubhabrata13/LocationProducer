d90714e936038fb28d2f25e355096e69ee955431
Shubhabrata De1 <119049720+shubhabrata-de@users.noreply.github.com>
2023-11-14 14:22:40 +0530

    commit
    commit

--- src/main/java/com/apmm/service/ProducerService.java
+++ src/main/java/com/apmm/service/ProducerService.java
@@ -7,6 +7,7 @@ import com.azure.storage.blob.models.ListBlobsOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
@@ -29,7 +30,8 @@ public final class ProducerService {
 	@Autowired
 	private ReactiveKafkaProducerTemplate<String, String> reactiveKafkaProducerTemplate;
 
-	private final String TOPIC = "location.poc";
+	@Value("${producer.topic.name}")
+	private String TOPIC;
 
 	public ProducerService(ReactiveKafkaProducerTemplate<String, String> reactiveKafkaProducerTemplate,BlobContainerAsyncClient blobContainerSourceClient) {
 		this.reactiveKafkaProducerTemplate = reactiveKafkaProducerTemplate;
